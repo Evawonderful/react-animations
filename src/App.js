3f45c7ba7cd56886f5ea122b3b6ab99dc13bb09b
earlpappi <earlpappi@gmail.com>
2023-05-22 11:49:27 +0100

    --commit: base setup gsap

--- src/App.js
+++ src/App.js
@@ -1,47 +1,134 @@
-import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faInstagramSquare, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons'
-
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faInstagramSquare,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import { useEffect, useRef } from "react";
+import gsap, { Power3 } from "gsap";
 
 function App() {
+  const ease = Power3.easeOut;
+  const newRef = useRef();
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      newRef.current = gsap
+        .timeline()
+        .from(
+          ".logoItem",
+          1,
+          {
+            opacity: 0,
+            x: -20,
+          },
+          "+=.1"
+        )
+        .from(
+          [".menuItems", ".menuItemIcon"],
+          0.5,
+          {
+            opacity: 0,
+            y: -20,
+            stagger: {
+              amount: 0.5,
+            },
+          },
+          "+=.1"
+        )
+        .from(
+          [".heroTexts"],
+          0.5,
+          {
+            opacity: 0,
+            y: 15,
+            skewX: 10,
+            stagger: {
+              amount: 0.7,
+              ease: ease,
+            },
+            ease: ease,
+          },
+          "+=.1"
+        )
+        .from(
+          "imgCon",
+          0.5,
+          {
+            opacity: 0,
+            x: 20,
+          },
+          ease,
+          "+=0"
+        );
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div className="App">
+    <div className="w-10/12 mx-auto">
+      <nav className="p-5">
+        <div className="flex justify-between items-center">
+          <img
+            src="assets/logo.png"
+            width="100"
+            height="100"
+            className="logoItem"
+          />
 
-      <nav className='p-5' >
-        <div className='flex justify-between'>
-          <img src="assets/logo.png" width="100" height="100"/>
-
-          <ul className='flex justify-between gap-5 mt-3 text-lg font-medium'>
-            <li>Products</li>
-            <li>Our Story</li>
-            <li>Blog</li>
-            <li>Find Store</li>
+          <ul className="flex justify-between gap-5 mt-3 text-lg font-medium">
+            <li className="menuItems">Products</li>
+            <li className="menuItems">Our Story</li>
+            <li className="menuItems">Blog</li>
+            <li className="menuItems">Find Store</li>
           </ul>
 
-          <span className='flex justify-between gap-2 h-fit '>
-            <span className='border border-black rounded-full w-9 h-9 '><FontAwesomeIcon icon={faFacebookF} className='p-2 pl-3 transition ease-in-out delay-300 duration-300 hover:translate-y-3' /></span>
-            <span className='border border-black rounded-full w-9 h-9 text-center'><FontAwesomeIcon icon={faTwitter} className='p-2'/></span>
-            <span className='border border-black rounded-full w-9 h-9 text-center'><FontAwesomeIcon icon={faYoutube} className='p-2 '/></span>
-            <span className='border border-black rounded-full w-9 h-9 text-center'><FontAwesomeIcon icon={faInstagram} className='p-2'/></span>
-
+          {/* icon caontainer */}
+          <span className="flex justify-between gap-2 h-fit ">
+            <span className="border border-black rounded-full w-9 h-9 text-center menuItemIcon">
+              <FontAwesomeIcon icon={faFacebookF} className="p-2" />
+            </span>
+            <span className="border border-black rounded-full w-9 h-9 text-center menuItemIcon">
+              <FontAwesomeIcon icon={faTwitter} className="p-2" />
+            </span>
+            <span className="border border-black rounded-full w-9 h-9 text-center menuItemIcon">
+              <FontAwesomeIcon icon={faYoutube} className="p-2 " />
+            </span>
+            <span className="border border-black rounded-full w-9 h-9 text-center menuItemIcon">
+              <FontAwesomeIcon icon={faInstagram} className="p-2" />
+            </span>
           </span>
         </div>
-        
       </nav>
 
-      <div className='w-[50%] ml-[30%]'>
-        <div className='text-4xl'>At Welch’s®,</div>
-        <div className='text-8xl font-bold'> We Put The Fruit <br/> In Fruit Snacks®</div>
-        <div className='text-xl my-7'>Welch’s® are America’s Favorite Fruit <br/> Snacks®, made with Real Fruit delivering <br/> delicious taste the whole family loves.</div>
-    
+      <div className="flex justify-between items-center py-[5%]">
+        <div className="w-[60%]">
+          <p className="text-4xl heroTexts">At Welch’s®,</p>
+          <p className="text-8xl font-bold heroTexts">
+            {" "}
+            We Put The Fruit <br /> In Fruit Snacks®
+          </p>
+          <p className="text-xl flex flex-col gap-2 my-7">
+            <span className="heroTexts">
+              Welch’s® are America’s Favorite Fruit
+            </span>
+            <span className="heroTexts">
+              Snacks®, made with Real Fruit delivering
+            </span>
+            <span className="heroTexts">
+              delicious taste the whole family loves.
+            </span>
+          </p>
+        </div>
+
+        <div className="w-[40%] imgCon">
+          <img src="assets/frolic2.png" />
+        </div>
       </div>
-
-      <img src="assets/frolic1.png" width="400" height="400" className='animate-bounce-slow right-0 top-50 absolute'/>
-      <img src="assets/frolic2.png" width="400" height="400" className='animate-bounce-slow duration-600 left-0 top-44 absolute'/>
-      <img src="assets/mayobowl.jpg" width="100" height="100"/> 
-      <img src="assets/tomatoes.png" width="300" height="400" className='animate-bounce-slow right-0 top-44 absolute'/> 
-
-     
     </div>
   );
 }
