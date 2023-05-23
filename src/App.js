2af538a6c11a835cb0c51c92d9205c3ce46b77c6
earlpappi <earlpappi@gmail.com>
2023-05-23 13:41:03 +0100

    --commit: react-reveal

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
             ease,
           },
           "+=.1"
-        );
+        )
     });
 
     return () => ctx.revert();
--- src/components/ColorChange.js
+++ src/components/ColorChange.js
@@ -1,16 +1,17 @@
-import React, { useLayoutEffect, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Fade, Slide } from "react-reveal";
 
 // import "./style.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 const ColorChange = () => {
   let revealsRef = useRef([]);
   revealsRef.current = [];
+
+  let animRef = useRef();
 
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
@@ -39,26 +40,103 @@ const ColorChange = () => {
             //     backgroundColor: prevBg,
             //   }),
           },
-        })
-
-        gsap.to(".frolic-pepe", {
-          scrollTrigger: {
-            trigger: ".frolic-pepe",
-            toggleActions: "restart pause resume none",
-          },
-          rotation: 360,
-          duration: 2,
-          onComplete: function() {
-            gsap.to(".frolic-pepe", {
-              x: -100,
-              duration: 3,
-            });
-          }
         });
+
+        // gsap.to(".frolic-pepe", {
+        //   scrollTrigger: {
+        //     trigger: ".frolic-pepe",
+        //     toggleActions: "restart pause resume none",
+        //   },
+        //   rotation: 360,
+        //   duration: 2,
+        //   onComplete: function () {
+        //     gsap.to(".frolic-pepe", {
+        //       x: -100,
+        //       duration: 3,
+        //     });
+        //   },
+        // });
+
+        // gsap.to(".frolic-pepe", {
+        //   rotation: 360,
+        //   scrollTrigger: {
+        //     trigger: ".frolic-pepe",
+        //     toggleActions: "restart pause resume none",
+        //     start: "top 80%",
+        //     end: "bottom 20%",
+        //     onEnter: function () {
+        //       gsap.to(".frolic-pepe", { x: -100, duration: 3 });
+        //     },
+        //     onLeaveBack: function () {
+        //       gsap.to(".frolic-pepe", { x: -100, duration: 0 });
+        //     },
+        //     onLeave: function () {
+        //       gsap.to(".frolic-pepe", { x: 100, duration: 0 });
+        //     },
+        //   },
+        //   duration: 2,
+        // });
       });
     });
     return () => ctx.revert();
   }, []);
+
+  const newRef = useRef();
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      newRef.current = gsap.timeline().to(".frolic-pepe", {
+        rotation: 360,
+        scrollTrigger: {
+          trigger: ".frolic-pepe",
+          toggleActions: "restart pause resume none",
+          start: "top 80%",
+          end: "bottom 20%",
+          onEnter: function () {
+            gsap.to(".frolic-pepe", { x: -100, duration: 3 });
+          },
+          onLeaveBack: function () {
+            gsap.to(".frolic-pepe", { x: -100, duration: 0 });
+          },
+          onLeave: function () {
+            gsap.to(".frolic-pepe", { x: 100, duration: 0 });
+          },
+        },
+        duration: 2,
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
+  function animateFrolicPepe() {
+    gsap.to(".frolic-pepe", {
+      rotation: 360,
+      scrollTrigger: {
+        trigger: ".frolic-pepe",
+        toggleActions: "restart pause resume none",
+        start: "top 80%", // Adjust the value as needed
+        end: "bottom 20%", // Adjust the value as needed
+        onEnter: function () {
+          gsap.to(".frolic-pepe", { x: -100, duration: 3 });
+        },
+        onLeaveBack: function () {
+          gsap.to(".frolic-pepe", { x: -100, duration: 0 });
+        },
+        onLeave: function () {
+          gsap.to(".frolic-pepe", { x: 100, duration: 0 });
+        },
+      },
+      duration: 2,
+    });
+  }
+
+  // animateFrolicPepe()
+
+  // Call the function to trigger the animation
+  animateFrolicPepe();
+
+  useEffect(() => {});
 
   const addToRef = (el) => {
     if (el && !revealsRef.current.includes(el)) {
@@ -89,19 +167,24 @@ const ColorChange = () => {
       <div
         data-bgcolor="#b45309"
         data-text=""
-        className="h-screen w-full pannel text-pink-900 justify-center items-center flex text-5xl "
+        className="h-screen w-10/12 mx-auto pannel text-pink-900 justify-center items-center flex text-5xl "
       >
         <div>
-          <h1 className="font-bold text-2xl">Meals love pepe </h1>
-          <span>...pepper it up</span>
+          <Fade left cascade>
+            <h1 className="font-bold text-2xl">Meals love pepe </h1>
+            <span className="text-6xl">...pepper it up</span>
+          </Fade>
         </div>
 
-        <img
-          src="assets/frolicpepe.png"
-          width="600"
-          height="600"
-          className="frolic-pepe"
-        />
+        <div>
+          <Slide top>
+            <img
+              src="assets/frolicpepe.png"
+              width="600"
+              height="600"
+              className="frolic-pepe"
+            /></Slide>
+        </div>
       </div>
     </div>
   );
